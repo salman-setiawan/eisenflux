c04bdeef133e8dd1641e1c857b86df52f8e959e2
salman-setiawan <salmansetiawan88@gmail.com>
2025-09-18 13:52:35 +0800

    rework article cover

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -15,51 +15,51 @@ const Article = () => {
     return <Notfound />;
   }
 
-  const { title, cover, categories, extUrl, extImg, extText, contents } = selectedArticle;
+  const { title, categories, extUrl, extImg, extText, contents } = selectedArticle;
 
   return (
     <div>
-      <div className="flex place-content-center pt-2">
-        <div className="px-5 flex flex-col items-center gap-x-8 gap-y-4 w-full md:max-w-[640px] pb-12">
-          <div className="flex flex-col gap-y-2">
-            <div className="w-full flex justify-end py-4">
-              <button
-                onClick={toggleLanguage}
-                className="text-[15px] h-fit underline underline-offset-1 text-[#ffa500]"
-              >
-                {language === "en" ? "English" : "Bahasa"}
-              </button>
-            </div>
-            <div
-              className="rounded-xl h-[320px] bg-cover bg-center"
-              style={{ backgroundImage: `url(${cover})` }}
-            ></div>
-            <div className="pt-2 pb-6 flex flex-col gap-y-6">
-              <div className='pb-1'>
-                <div className="text-2xl font-semibold leading-relaxed text-justify">
-                  {title}
-                </div>
-                <div className="flex flex-wrap gap-x-2 text-sm font-semibold pt-1.5">
-                  {categories.map((cat, i) => (
-                    <span className='py-1 px-2 bg-opacity-15 bg-[#ffaa00] rounded-lg' key={i}>{cat[language]}</span>
-                  ))}
-                </div>
-              </div>
-              {extUrl && (
-                <div className="p-0.5 snake">
-                  <a href={extUrl} target="_blank" rel="noopener noreferrer">
-                    <Button img={extImg} bg="bg-[#111111]" text={extText[language]} hoverBg="hover:bg-[#ffaa00]" />
-                  </a>
-                </div>
-              )}
-            </div>
-
-            {contents.map((content, index) => (
-              <div key={index}>
-                <Content data={content} language={language} />
-              </div>
-            ))}
+      <div className="flex flex-col pt-2 items-center">
+        <div className="px-5 flex flex-col gap-y-2 justify-center w-full">
+          <div className="w-full flex justify-end py-4">
+            <button
+              onClick={toggleLanguage}
+              className="text-[15px] h-fit underline underline-offset-1 text-[#ffa500]"
+            >
+              {language === "en" ? "English" : "Bahasa"}
+            </button>
           </div>
+        </div>
+        <div
+          className="w-full h-[580px] md:h-[320px] bg-cover bg-center bg-neutral-900"
+          // style={{ backgroundImage: `url(${cover})` }}
+        ></div>
+        <div className="px-5 flex flex-col gap-y-2 w-full md:max-w-[640px] pt-6 pb-12">
+          <div className="pt-2 pb-6 flex flex-col gap-y-6">
+            <div className='pb-1'>
+              <div className="text-2xl font-semibold leading-relaxed text-justify">
+                {title}
+              </div>
+              <div className="flex flex-wrap gap-x-2 text-[12px] font-semibold pt-1.5">
+                {categories.map((cat, i) => (
+                  <span className='py-1 px-2 bg-opacity-15 bg-[#ffaa00] rounded-lg' key={i}>{cat[language]}</span>
+                ))}
+              </div>
+            </div>
+            {extUrl && (
+              <div className="p-0.5 snake">
+                <a href={extUrl} target="_blank" rel="noopener noreferrer">
+                  <Button img={extImg} bg="bg-[#111111]" text={extText[language]} hoverBg="hover:bg-[#ffaa00]" />
+                </a>
+              </div>
+            )}
+          </div>
+
+          {contents.map((content, index) => (
+            <div key={index}>
+              <Content data={content} language={language} />
+            </div>
+          ))}
         </div>
       </div>
       <div
