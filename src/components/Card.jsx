9df3c882d2a9b7e9b57332d56a4314f12f115b03
salman-setiawan <salmansetiawan88@gmail.com>
2025-10-25 00:19:25 +0800

    fixing card img bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -87,7 +87,7 @@ const Card = ({ title, nav, desc, categories, img, intImg, intText, intImg2, int
           </div>
           <div className="relative overflow-hidden w-full">
             {title && (
-              <motion.h1 variants={container} initial="initial" animate="animate" className="font-anton absolute inset-0 flex flex-col leading-none text-center items-center justify-center text-[18rem] xl:text-[24rem] font-black text-white/10 rotate-[-28deg] uppercase select-none">
+              <motion.h1 variants={container} initial="initial" animate="animate" className="font-anton absolute inset-0 flex flex-col leading-none text-center items-center justify-center text-[18rem] xl:text-[24rem] font-black text-white/13 rotate-[-28deg] uppercase select-none">
                 {words.map((word, wi) => (
                   <motion.div key={wi} variants={wordVariant} className="flex">
                     {word.split('').map((char, ci) => (
