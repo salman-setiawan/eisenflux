89bbeec9d78b0db6e89cb58cc128ff97eba4b4da
salman-setiawan <salmansetiawan88@gmail.com>
2025-11-07 17:18:35 +0800

    .

--- src/components/WorkChip.jsx
+++ src/components/WorkChip.jsx
@@ -3,7 +3,7 @@ const WorkChip = ({ label, color }) => {
     <div>
       <div className="flex gap-x-1.5 w-fit items-center">
         <div className="w-[10px] h-[10px] rounded-full animate-blink"></div>
-        <div className={`text-relaxed text-[12px] font-semibold ${color}`}>{label}</div>
+        <div className={`text-[12px] font-semibold ${color}`}>{label}</div>
       </div>
     </div>
   )
