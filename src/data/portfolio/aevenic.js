5d0ef12738dc97dd6c168b1013f7d75adad27ac8
salman-setiawan <salmansetiawan88@gmail.com>
2026-02-18 15:48:32 +0800

    adding aevenic freelance project

---
+++ src/data/portfolio/aevenic.js
@@ -0,0 +1,25 @@
+const Duitra = {
+  card: {
+    id: 4,
+    slug: 'aevenic-studio',
+    title: 'FREELANCE PROJECT',
+    nav: 'aevenic_studio.prj',
+    cover: '/bg.webp',
+    obj: '/showcase/aevenic.png',
+    desc: {
+      en: "a small project that i have for help build a fast and clear website that attracts customers and grow the business.",
+      id: 'sebuah proyek kecil yang saya lakukan untuk membantu membuat situs web yang cepat dan jelas yang menarik pelanggan dan meningkatkan bisnis.'
+    },
+    categories: [
+      { en: 'Product Design', id: 'Desain Produk' },
+      { en: 'Freelance Project', id: 'Proyek Freelance' },
+      { en: 'Independent UI/UX', id: 'UI/UX Independen' },
+      { en: 'Web Landing Page', id: 'Halaman Arahan Web' },
+      { en: 'Live Site', id: 'Situs Langsung' },
+    ],
+    extUrl: 'https://aevenic.netlify.app',
+    extText: { en: 'See Website', id: 'Lihat Situs Web' },
+  },
+};
+
+export default Duitra;
